bdae3e5601dbdd475e809fbc7a4efe235585293c
Washington Nieto <washington_nieto@hotmail.com>
2021-01-13 15:39:41 -0500

    actualizacion login

--- proyecto-php/includes/pie.php
+++
@@ -1,8 +0,0 @@
-            <div class="clearfix"></div>
-        </div>
-
-        <footer id="pie">
-            <p>Desarrollado por Washington Nieto &COPY;2020 </p>
-        </footer>
-    </body>
-</html>
