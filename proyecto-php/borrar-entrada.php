b0fa12eca33ed67957cad26f11e4d5ff13c98508
Washington Nieto <washington_nieto@hotmail.com>
2021-01-22 10:53:49 -0500

    borrar-entradas

--- proyecto-php/borrar-entrada.php
+++ proyecto-php/borrar-entrada.php
@@ -1,18 +1,9 @@
+<?php require_once 'includes/conexion.php'; ?>
+<?php require_once 'includes/helpers.php'; ?>
+
 <?php
-require_once 'includes/conexion.php';
-
-
-//if(isset($_SESSION['usuario']) && isset($_GET['id'])){
-if(isset($_SESSION['usuario'])){
-    $entrada_id = $_GET['id'];
-    $usuario_id = $_SESSION['usuario']['id'];
-    var_dump($entrada_id);
-    //echo mysqli_error($db);
+    $entrada_actual = conseguirEntrada1($db, $_GET['id']);
+    var_dump($_GET['id']);
     die();
     
-    //$sql = "DELETE FROM entradas WHERE usuario_id = $usuario_id AND id = $entrada_id";
-    //$borrar = mysqli_query($db, $sql);
-}else{
-    header("Location: mis-datos.php");
-}
-header("Location: index.php");
+?>
