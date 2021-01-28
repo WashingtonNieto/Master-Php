eef5da2245ae50f11e72f37e64bb5d9409d56be5
Washington Nieto <washington_nieto@hotmail.com>
2021-01-28 15:15:08 -0500

    constructor

--- aprendiendo-php-poo/01-clases/index.php
+++ aprendiendo-php-poo/01-clases/index.php
@@ -1,9 +1,67 @@
-<h1>hola mundo</h1>
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Programacion Orientada a Objetos
+
+//Clase : es un molde para crear mas objetos con las mismas caracteristicas
+
+
+Class coche{
+    
+    //Atributos o propiedades ( variables)
+    
+    public $color = "Rojo";
+    public $marca = "Ferrari";
+    public $modelo = "Aventador";
+    public $velocidad = 300;
+    public $caballaje = 500;
+    public $plazas = 2;
+    
+    //Metodos: Son acciones que hace el objeto (funciones)
+    public function getColor(){
+        //busca en esta clase la propiedad x
+        return $this->color;
+    }
+    
+    public function setColor($color){
+        $this->color = $color;
+    }
+    
+    public function setModelo($modelo){
+        $this->modelo = $modelo;
+    }
+    
+    public function acelerar(){
+        $this->velocidad++;
+    }
+    
+    public function frenar(){
+        $this->velocidad--;
+    }
+    
+    public function getVelocidad(){
+        return $this->velocidad;
+    }
+}//fin definicion de clase
+
+//crear un objeto
+$coche = new Coche();
+
+//usar los metodos
+
+$coche->setColor("Amarillo");
+echo "El color del coche es: ".$coche->getColor().'</br>';
+
+$coche->acelerar();
+$coche->acelerar();
+$coche->acelerar();
+$coche->frenar();
+
+echo "La velocidad del coche es: ".$coche->getVelocidad().'</br>';
+
+$coche2 = new Coche();
+$coche2->setColor("Verde");
+$coche2->setModelo("Gallardo");
+
+var_dump ($coche);
+var_dump ($coche2);
 
---
+++ aprendiendo-php-poo/constructor/coche.php
@@ -0,0 +1,86 @@
+<?php
+//contructor: Es un metodo especial dentro de una clase, y se suele utilizar
+//para darle un valor a los atributos o propiedades que tiene un objeto 
+//el constructor es el primer metodo que se ejecuta al crea el objeto y se
+//llama automaticamente al crearlo.
+//este metodo puede recibir parametros como cualquier otro metodo y para pasarselo
+//simpliemente tenemos pasarle parametros a la instancia del objeto.
+//El contructor nunca devuelve ningun dato, simplemente es un metodo magico que
+//se encarga de setear informacion en las propiedades. se ejecuta de primero
+//
+Class coche{
+    
+    //Atributos o propiedades ( variables)
+    
+    //public: podemos acceder a el desde cualquier lugar dentro de la clase actual
+    //        dentro de clases que hereden esta clase o fuera de la clase
+    public $color;
+    
+    //protected:Podemos acceder a ellos desde la clase que los define y desde clase
+    //que hereden esta clase
+    protected $marca;
+    
+    //Private: unicamente se puede acceder desde esta clase
+    private $modelo;
+    
+    //public $color;
+    //public $marca;
+    //public $modelo;
+    public $velocidad;
+    public $caballaje;
+    public $plazas;
+    
+    public function __construct($color,$marca,$modelo,$velocidad,$caballaje,$plazas) {
+        $this->color = $color;
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->velocidad = $velocidad;
+        $this->caballaje = $caballaje;
+        $this->plazas = $plazas;
+    }
+    
+    //Metodos: Son acciones que hace el objeto (funciones)
+    public function getColor(){
+        //busca en esta clase la propiedad x
+        return $this->color;
+    }
+    
+    public function setColor($color){
+        $this->color = $color;
+    }
+    
+    public function setModelo($modelo){
+        $this->modelo = $modelo;
+    }     
+    
+    public function getModelo($modelo){
+       return $this->modelo;
+    }    
+    
+    public function setMarca($marca){
+        $this->marca = $marca;
+    }
+    
+    public function acelerar(){
+        $this->velocidad++;
+    }
+    
+    public function frenar(){
+        $this->velocidad--;
+    }
+    
+    public function getVelocidad(){
+        return $this->velocidad;
+    }
+    
+    public function mostrarInformacion($miObjeto){
+        $informacion = "<h1>Informacion del coche</h1>";
+        $informacion .= "Color: ".$miObjeto->color;
+        $informacion .= "<br/>Modelo: ".$miObjeto->modelo;
+        $informacion .= "<br/>Velocidad: ".$miObjeto->velocidad;
+        
+        return $informacion;
+    }
+
+}//fin definicion de clase
+
