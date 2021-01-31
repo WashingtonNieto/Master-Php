95e0ad3da37caea3d8202ffb17cd30c0b91b7ff5
Washington Nieto <washington_nieto@hotmail.com>
2021-01-31 08:05:29 -0500

    generar-pdf

--- Aprendiendo-librerias-php/generar-pdf/index.php
+++ Aprendiendo-librerias-php/generar-pdf/index.php
@@ -1,10 +1,9 @@
 <?php
-
 require '../vendor/autoload.php';
 
-use Spipu\Html2Pdf\Html2Pdf;
+use spipu\html2pdf\html2pdf;
 
-$html2pdf = new Html2Pdf();
+$html2pdf = new html2pdf();
 
 $html = "<h1>Hola Mundo desde una libreria de PHP para hacer PDFs</h1>";
 $html .= "<p>Master en PHP</p>";
