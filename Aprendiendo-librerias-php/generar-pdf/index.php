b8af74c02fade206d407b3aa7b35ed26afc96a33
Washington Nieto <washington_nieto@hotmail.com>
2021-01-31 07:31:55 -0500

    generar-pdf

--- Aprendiendo-librerias-php/generar-pdf/index.php
+++ Aprendiendo-librerias-php/generar-pdf/index.php
@@ -1,8 +1,13 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require '../vendor/autoload.php';
 
+use Spipu\Html2Pdf\Html2Pdf;
+
+$html2pdf = new Html2Pdf();
+
+$html = "<h1>Hola Mundo desde una libreria de PHP para hacer PDFs</h1>";
+$html .= "<p>Master en PHP</p>";
+
+$html2pdf->writeHTML($html);
+$html2pdf->Output('pdf_generado.pdf');
