982acc14fa8ec01b2815d850c2c6a53d00a44f29
Washington Nieto <washington_nieto@hotmail.com>
2021-02-07 06:46:26 -0500

    Productos borrado

--- proyecto-php-poo/views/producto/gestion.php
+++ proyecto-php-poo/views/producto/gestion.php
@@ -4,10 +4,20 @@
     Crear producto
 </a>
 
+//Cerrar la creacion y borrar la sesion
 <?php if(isset($_SESSION['producto']) && $_SESSION['producto'] == 'complete'): ?>
     <strong class="alert_green">El producto fue creado correctamente</strong>
 <?php elseif(isset($_SESSION['producto']) && $_SESSION['producto'] == 'complete'): ?>
     <strong class="alert_red">El producto NO fue creado correctamente</strong>
+<?php endif; ?>
+
+<?php Utils::deleteSession('producto'); ?>
+
+/*Cerrar la eliminacion y borrar la sesion*/
+<?php if(isset($_SESSION['delete']) && $_SESSION['delete'] == 'complete'): ?>
+    <strong class="alert_green">El producto fue eliminado correctamente</strong>
+<?php elseif(isset($_SESSION['delete']) && $_SESSION['delete'] == 'complete'): ?>
+    <strong class="alert_red">El producto NO fue eliminado correctamente</strong>
 <?php endif; ?>
 
 <?php Utils::deleteSession('producto'); ?>
@@ -19,6 +29,7 @@
         <th>NOMBRE</th>
         <th>PRECIO</th>
         <th>STOCK</th>
+        <th>ACCIONES</th>
     </tr>
     <?php while($pro = $productos->fetch_object()): ?>
         <tr>
@@ -26,6 +37,10 @@
             <td><?=$pro->nombre; ?></td>
             <td><?=$pro->precio; ?></td>
             <td><?=$pro->stock; ?></td>
+            <td>
+                <a href="<?=base_url?>producto/editar&id=<?=$pro->id?>" class="button button-gestion">Editar</a>
+                <a href="<?=base_url?>producto/eliminar&id=<?=$pro->id?>" class="button button-gestion button-red">Eliminar</a>
+            </td>
         </tr>
     <?php endwhile; ?>
 </table>
