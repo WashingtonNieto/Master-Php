ae3078f895b89def71cc501e471449df93911896
Washington Nieto <washington_nieto@hotmail.com>
2021-02-12 10:53:22 -0500

    Pedidos

--- proyecto-php-poo/database/database.sql
+++ proyecto-php-poo/database/database.sql
@@ -70,7 +70,8 @@ CONSTRAINT fk_pedido_usuario FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
 CREATE TABLE lineas_pedidos(
 id          int(255) auto_increment not null,
 pedido_id   int(255) not null,
-producto_id int(255) not null,
+producto_id int(255) not null
+unidades    int(255) not null,
 CONSTRAINT pk_lineas_pedidos PRIMARY KEY(id),
 CONSTRAINT fk_linea_pedido FOREIGN KEY(pedido_id) REFERENCES pedidos(id),
 CONSTRAINT fk_linea_producto FOREIGN KEY(producto_id) REFERENCES productos(id)
