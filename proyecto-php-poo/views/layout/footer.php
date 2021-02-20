5d933a0cca546a1d5b5721fd361481d649627236
Washington Nieto <washington_nieto@hotmail.com>
2021-02-20 16:22:20 -0500

    Update footer.php

--- proyecto-php-poo/views/layout/footer.php
+++ proyecto-php-poo/views/layout/footer.php
@@ -3,7 +3,7 @@
 
 <!-- PIE DE PÁGINA -->
 <footer id="footer">
-    <p>Desarrollado por Ing. Washington Nieto Arce WEB &copy; <?= date('Y') ?></p>
+    <p>Desarrollado por Washington Nieto Arce WEB &copy; <?= date('Y') ?></p>
 </footer>
 </div>
 </body>
